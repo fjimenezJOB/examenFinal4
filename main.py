2897e7f543631aaad94b7541812fd1db4f5e832b
fjimenezJOB <fjimenezjob@gmail.com>
2019-10-21 10:04:01 +0200

    Funciones añadidas, en metodos.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_wtf import CSRFProtect
-import form
-import csv
+from metodos import Logica
 
 app = Flask(__name__)
 app.secret_key = 'examen_final'
 csrf = CSRFProtect(app)
+game = Logica()
 
 
 @app.route('/')
@@ -15,9 +15,10 @@ def home():
 
 @app.route('/enviar', methods=['POST', 'GET'])
 def enviar():
+    elegidas = request.form.getlist('opciones')
+    suerte = game.random_opciones(elegidas)
 
-
-    return render_template('index.html')
+    return render_template('game.html', suerte = suerte)
 
 
 if __name__ == "__main__":
--- metodos.py
+++ metodos.py
@@ -1,15 +1,30 @@
+import random
+import time
+
+gajos = ['1000€', '500€', 'PIERDE', '200€',
+         '-50% Dinero', 'CALABOZO', 'DINERO X2']
 
 class Logica:
 
-    def __init__(self):
-        pass
-
+        # Metodo que escribe el score en el archivo txt
     def escribir(self, nombre, score):
+
+        ahora = time.strftime("%c")  # Fecha y hora actuales
         with open('scores.txt', 'w') as f:
-            f.write(f'{nombre},{score}\n')
+            f.write(f'{nombre},{score},{ahora}\n')
         f.close()
 
+
+        # Metodo que lee el txt
     def leer(self):
         with open('scores.txt', 'r') as f:
             for line in f:
                 print(line)
+
+        # Metodo que saca un gajo random y le añade los que el usuario haya elegido
+    def random_opciones(self, opciones=[]):
+        for opcion in range(6):
+            gajos.append(opciones[opcion])
+        elegido = gajos[random.randint(0, 13)]
+        print (gajos)
+        return elegido
